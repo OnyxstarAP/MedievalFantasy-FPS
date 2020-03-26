89ecc7c3774c684cd45dc8323089f7101030db11
OnyxstarAP <60722208+OnyxstarAP@users.noreply.github.com>
2020-03-25 20:32:01 -0400

    Fixed UI & Revampign Menu
    - Removed Retical from UI
    - Added base for new Main Menu

---
+++ Assets/Andre Assets/Andre Scripts/UI, Settings & Menu Scripts/MainMenuRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRotate : MonoBehaviour
+{
+
+    public Transform focusObject;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(focusObject);
+        transform.Translate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+}
