7d51e54e76203214b818eba6dc1293920a44ed87
OnyxstarAP <60722208+OnyxstarAP@users.noreply.github.com>
2020-04-22 21:13:28 -0400

    4/22/20 Updated
    - Updated Enemy Sprites
    - Updated Menu

--- Assets/Andre Assets/Andre Scripts/UI, Settings & Menu Scripts/MainMenuRotate.cs
+++ Assets/Andre Assets/Andre Scripts/UI, Settings & Menu Scripts/MainMenuRotate.cs
@@ -9,11 +9,20 @@ public class MainMenuRotate : MonoBehaviour
 
     [SerializeField]
     private float rotateSpeed;
+    [SerializeField]
+    private float angleTimer;
+    [SerializeField]
+    private float xPosition;
+    [SerializeField]
+    private float yPosition;
+    [SerializeField]
+    private float zPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        reRandomizer();
+        StartCoroutine(cameraPositionSwitch());
     }
 
     // Update is called once per frame
@@ -21,5 +30,22 @@ public class MainMenuRotate : MonoBehaviour
     {
         transform.LookAt(focusObject);
         transform.Translate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator cameraPositionSwitch()
+    {
+        yield return new WaitForSeconds(angleTimer);
+        reRandomizer();
+        StartCoroutine(cameraPositionSwitch());
+    }
+
+    private void reRandomizer()
+    {
+        angleTimer = Mathf.Round(Random.Range(4,7.5f));
+        //rotateSpeed = Random.Range(0.25f, 0.75f);
+        //xPosition = Random.Range(Random.Range(-1, -3), Random.Range(1, 3));
+        yPosition = Random.Range(0.25f, 1);
+       // zPosition = Random.Range(Random.Range(-1, -3), Random.Range(1, 3));
+        transform.position = new Vector3(0, transform.position.y, transform.position.z);
     }
 }
